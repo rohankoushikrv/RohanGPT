9236adba779277f3aa39703b6a4ca52daa42d99d
Rohan Koushik Gajulapalle <koushik.gajulapalle22@gmail.com>
2025-06-14 22:50:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,6 @@
-const API_KEY = 'AIzaSyB_lP7hIYkIF899gcx8yJleInuWpjqfXM8'; // This is risky, but can work for frontend apps
-export { API_KEY };
+// config.js
+const config = {
+    apiKey: "AIzaSyB_lP7hIYkIF899gcx8yJleInuWpjqfXM8"
+};
+
+export default config;
